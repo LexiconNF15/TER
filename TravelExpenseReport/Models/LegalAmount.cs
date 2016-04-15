c890a0422bb0772d842dce5b71ecf93866469976
Lena Källgren <lkallgren@live.se>
2016-04-15 16:11:22 +0200

    Ändart LegalAmount (model), samt Seedat värden för den i Configuration.cs

--- TravelExpenseReport/Models/LegalAmount.cs
+++ TravelExpenseReport/Models/LegalAmount.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +10,16 @@ namespace TravelExpenseReport.Models
     public class LegalAmount
     {
         public int LegalAmountId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime ValidDate { get; set; }
-        public int FullDayAmount { get; set; }
-        public int HalfDayAmount { get; set; }
-        public int NightAmount { get; set; }
-        public float MilageAmount { get; set; }
+        [DisplayName("Heldagstraktamente")]
+        public int? FullDayAmount { get; set; }
+        [DisplayName("Halvdagstraktamente")]
+        public int? HalfDayAmount { get; set; }
+        [DisplayName("Natttraktamente")]
+        public int? NightAmount { get; set; }
+        [DisplayName("Kilometerbelopp")]
+        public int? MilageAmount { get; set; }
     }
 }
