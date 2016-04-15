123ac82da20998d1d71ad39971b5e876786204be
Lena Källgren <lkallgren@live.se>
2016-04-15 16:46:25 +0200

    pågående layoutförändringar mm för vyer för Exxåenses

--- TravelExpenseReport/Models/Expense.cs
+++ TravelExpenseReport/Models/Expense.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,7 @@ namespace TravelExpenseReport.Models
         public string ExpenseInformation { get; set; }
 
         [DisplayName("Datum")]
+        [DataType(DataType.Date)]
         public DateTime ExpenseDate { get; set; }
 
         [DisplayName("Kostnad")]
