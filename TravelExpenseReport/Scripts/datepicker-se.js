a738454b28691fca78ba50797d48f4f44dd24918
Lena Källgren <lkallgren@live.se>
2016-06-28 11:34:51 +0200

    Har lagt till Datepicker för Expenses för Webbläsare: IE och Firefox. Har skapat javascript med till en kalender för ovan datepicker på svenska att användas av IE och FireFox  ( datepicker-se.js)

---
+++ TravelExpenseReport/Scripts/datepicker-se.js
@@ -0,0 +1,47 @@
+﻿(function (factory) {
+    if (typeof define === "function" && define.amd) {
+
+        // AMD. Register as an anonymous module.
+        define(["../widgets/datepicker"], factory);
+    } else {
+
+        // Browser globals
+        factory(jQuery.datepicker);
+    }
+}(function (datepicker) {
+
+    datepicker.regional.se = {
+        closeText: "Stäng",
+        prevText: "&#xAB;Föregående",
+        nextText: "Nästa&#xBB;",
+        currentText: "I dag",
+        monthNames: [
+            "januari",
+            "februari",
+            "mars",
+            "april",
+            "maj",
+            "juni",
+            "juli",
+            "augusti",
+            "september",
+            "oktober",
+            "november",
+            "december"
+        ],
+        monthNamesShort: ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "okt", "nov", "dec"],
+        dayNamesShort: ["sön", "mån", "tis", "ons", "tor", "fre", "lör"],
+        dayNames: ["söndag", "måndag", "tisdag", "onsdag", "torsdag", "fredag", "lördag"],
+        dayNamesMin: ["sö", "må", "ti", "on", "to", "fr", "lö"],
+        weekHeader: "vecka",
+        dateFormat: "dd.mm.yy",
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ""
+    };
+    datepicker.setDefaults(datepicker.regional.se);
+
+    return datepicker.regional.se;
+
+}));
